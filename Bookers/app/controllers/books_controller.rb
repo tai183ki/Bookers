601cf606c7610aaf73201057dc1083926cdd4b00
tai183ki <tai183ki.9656@gmail.com>
2020-11-23 10:02:02 +0000

    [Add] 投稿機能

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
@@ -8,6 +9,21 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path, notice: 'Successufully'
+    else
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
