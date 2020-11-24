4fbb90ad313de7c5f90a7130d98b1b44189906ab
tai183ki <tai183ki.9656@gmail.com>
2020-11-24 07:21:40 +0000

    [Add] 削除機能

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -22,15 +22,21 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
